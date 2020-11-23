a66ee7a46614804a555e3ab1cfbab41c5e62f546
Bejan Tiberiu <tiberiu.bejan98@gmail.com>
2020-11-23 22:17:14 +0200

    Second Commit

--- Project 14 - Book Finder App/script.js
+++ Project 14 - Book Finder App/script.js
@@ -1,33 +1,59 @@
+const searchForm = document.getElementById('search-form');
+const searchInput = document.querySelector('#search-form input');
 const url = 'https://www.googleapis.com/books/v1/volumes?';
+let searchQuery;
 
 // EVENT LISTENERS
-window.addEventListener('DOMContentLoaded', getBooks);
+searchForm.addEventListener('submit', getBooks);
+searchInput.addEventListener('keyup', getBooks);
 
-async function getBooks(){
-    const data = await fetchPreviewBooks();
 
-    const books = data.map((book, index) => ({
-        id: index,
-        author : book.volumeInfo.authors[0],
-        title: book.volumeInfo.title,
-        publisher : book.volumeInfo.publisher,
-        rating : book.volumeInfo.averageRating,
-        categories : book.volumeInfo.categories.join(', '),
-        description : book.volumeInfo.description,
-        thumbnail : book.volumeInfo.imageLinks.thumbnail,
-        previewLink : book.volumeInfo.previewLink,
-        storeLink : book.volumeInfo.infoLink
-    }));
+async function getBooks(e){
+    e.preventDefault();
+    searchQuery = searchInput.value;
+
+    if(searchQuery === ''){ return; }
+
+    const books = await fetchPreviewBooks(searchQuery);
+    const booksArr = books.map(book => {
+        const volumeInfo = book.volumeInfo;
+        const { title: volumeTitle, authors: volumeAuthor, description: volumeDescription, averageRating: volumeRating, imageLinks: volumeThumbnail, infoLink: volumeLink } = volumeInfo;
+
+        return {volumeTitle, volumeAuthor, volumeDescription, volumeRating, volumeThumbnail, volumeLink};
+    });
+
+    displayBooks(booksArr);
+}
+
+function displayBooks(books){
+    const books_div = document.querySelector('.container');
+
+    let bookHTML = books.map(book => `
+        <div class="book">
+            <div class="book-image">
+                <img src="${book.volumeThumbnail.thumbnail}" alt="${book.volumeTitle}">
+            </div>
+            <div class="book-details">
+                <h2 class="heading-two mb-small">${book.volumeTitle}</h2>
+                <p class="paragraph mb-small">${book.volumeAuthor}</p>
+                <button type="button" class="button primary-button">Details
+                    <i class="fas fa-info-circle"></i>
+                </button>
+            </div>
+        </div>
+    `).join('');
+
+    books_div.innerHTML = bookHTML;
 }
 
 // FUNCTION TO FETCH PREVIEW BOOKS
-async function fetchPreviewBooks(){
-    const response = await fetch(`${url}q=author:george+r+r+martin&maxResults=6`);
+async function fetchPreviewBooks(searchQuery){
+    const response = await fetch(`${url}q=${searchQuery}&maxResults=20`);
 
     try{
         const data = await response.json();
         return data.items;
     } catch(error) {
-        console.log(error)
+        console.log(error);
     }
 }
