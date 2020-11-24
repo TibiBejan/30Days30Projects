57be6e9e4b5a4d4b7d60692faa18e151585abe43
Bejan Tiberiu <tiberiu.bejan98@gmail.com>
2020-11-24 12:07:32 +0200

    Responsive  Layout

--- Project 14 - Book Finder App/script.js
+++ Project 14 - Book Finder App/script.js
@@ -1,25 +1,23 @@
 const searchForm = document.getElementById('search-form');
-const searchInput = document.querySelector('#search-form input');
 const url = 'https://www.googleapis.com/books/v1/volumes?';
 let searchQuery;
 
 // EVENT LISTENERS
 searchForm.addEventListener('submit', getBooks);
-searchInput.addEventListener('keyup', getBooks);
-
 
 async function getBooks(e){
     e.preventDefault();
-    searchQuery = searchInput.value;
+    const searchInput = document.querySelector('#search-form input');
+    searchQuery = searchInput.value.split(' ').join('+');
 
     if(searchQuery === ''){ return; }
 
     const books = await fetchPreviewBooks(searchQuery);
     const booksArr = books.map(book => {
         const volumeInfo = book.volumeInfo;
-        const { title: volumeTitle, authors: volumeAuthor, description: volumeDescription, averageRating: volumeRating, imageLinks: volumeThumbnail, infoLink: volumeLink } = volumeInfo;
+        const { title: volumeTitle, authors: volumeAuthor, description: volumeDescription, imageLinks: volumeThumbnail, infoLink: volumeLink } = volumeInfo;
 
-        return {volumeTitle, volumeAuthor, volumeDescription, volumeRating, volumeThumbnail, volumeLink};
+        return { volumeTitle, volumeAuthor, volumeDescription, volumeThumbnail, volumeLink };
     });
 
     displayBooks(booksArr);
@@ -28,19 +26,19 @@ async function getBooks(e){
 function displayBooks(books){
     const books_div = document.querySelector('.container');
 
-    let bookHTML = books.map(book => `
-        <div class="book">
-            <div class="book-image">
-                <img src="${book.volumeThumbnail.thumbnail}" alt="${book.volumeTitle}">
-            </div>
-            <div class="book-details">
-                <h2 class="heading-two mb-small">${book.volumeTitle}</h2>
-                <p class="paragraph mb-small">${book.volumeAuthor}</p>
-                <button type="button" class="button primary-button">Details
-                    <i class="fas fa-info-circle"></i>
-                </button>
-            </div>
+    const bookHTML = books.map(book => `
+    <div class="book">
+        <div class="book-image">
+            <img src="${book.volumeThumbnail.thumbnail}" src="${book.volumeTitle}">
         </div>
+        <div class="book-details">
+            <h2 class="heading-two mb-small">${book.volumeTitle}</h2>
+            <p class="paragraph mb-small">${book.volumeAuthor}</p>
+            <a href="${book.volumeLink}" target="blank"class="button primary-button">Details
+                <i class="fas fa-info-circle"></i>
+            </a>
+        </div>
+    </div>
     `).join('');
 
     books_div.innerHTML = bookHTML;
